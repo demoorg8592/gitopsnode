1411ec7208eab6969bf28f730ba24112c88ce7fd
raja1985 <raja1985@users.noreply.github.com>
2023-06-23 16:40:20 +0530

    Update App.js
    changing the message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
           Version{' '}
-          <span style={{ color: 'red' }}>Final Commit....!!!</span>
+          <span style={{ color: 'red' }}>Github Action Argocd Demo!!!</span>
         </div>
       </header>
     </div>
